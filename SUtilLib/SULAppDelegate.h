751cc69a40bc744f14e2ec6aad0ab5ff4e67792b
Sohel Dadia <Sohel@Sohels-MacBook-Air.local>
2012-06-30 11:20:38 -0700

    Initial Commit

---
+++ SUtilLib/SULAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SULAppDelegate.h
+//  SUtilLib
+//
+//  Created by Sohel Dadia on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SULAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SUtilLib/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SUtilLib
+//
+//  Created by Sohel Dadia on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SULAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SULAppDelegate class]));
+    }
+}
